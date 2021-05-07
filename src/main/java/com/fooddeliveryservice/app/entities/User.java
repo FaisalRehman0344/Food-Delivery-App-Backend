4887976591de8c994a4fca71acd4cf057e604422
FaisalRehman0344 <faisalarain737@gmail.com>
2021-05-07 00:14:11 -0700

    removed errors

--- src/main/java/com/fooddeliveryservice/app/entities/User.java
+++ src/main/java/com/fooddeliveryservice/app/entities/User.java
@@ -1,9 +1,5 @@
 package com.fooddeliveryservice.app.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
